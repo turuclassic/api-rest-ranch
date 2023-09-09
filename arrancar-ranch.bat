b3a8ae693cff0b284afca178fdb17a841f7ea1cc
turuclassic <martin.e.flores@gmail.com>
2023-09-09 16:13:57 -0300

    arrancar pc

--- arrancar-ranch.bat
+++ arrancar-ranch.bat
@@ -1,2 +1,2 @@
-start cmd.exe /k "cd C:\Program Files\MongoDB\Server\5.0\bin && mongod.exe --dbpath C:\data\db"
-start cmd.exe /k "cd C:\Users\Martin Flores\beta-ranch\api-rest-ranch && npm start"
+start cmd.exe /k "cd C:\Program Files\MongoDB\Server\7.0\bin && mongod.exe --dbpath C:\data\db"
+start cmd.exe /k "cd C:\Users\SuperUSER\proyectos-web\api-rest-ranch && npm start"
